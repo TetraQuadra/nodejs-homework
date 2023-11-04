608a0baf3e6d59ae5de272e2d4755868f5589133
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2023-11-04 07:50:33 +0200

    fix: 404 on getAll in case there is no stored data

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -1,12 +1,7 @@
-const createErrorMessage = require("../../helpers/createErrorMessage");
-
 const contacts = require("../../models/contacts");
 const listContacts = async (req, res, next) => {
   try {
     const body = await contacts.listContacts();
-    if (!body) {
-      throw createErrorMessage(404);
-    }
     res.status(200).json(body);
   } catch (error) {
     const { status = 500, message = "Internal server error" } = error;
