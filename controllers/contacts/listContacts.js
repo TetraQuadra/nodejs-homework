1669a139af8f2561bad9245176e161280bad7107
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2023-11-05 18:25:44 +0200

    feat: GET /contacts added pagination

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -1,9 +1,16 @@
 const Contact = require("../../models/contact");
+
 const listContacts = async (req, res, next) => {
   try {
-    console.log(req.body);
-    const body = await Contact.find({ owner: req.user }).exec();
-    res.status(200).json(body);
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 20;
+    const skip = (page - 1) * limit;
+    const contacts = await Contact.find({ owner: req.user })
+      .skip(skip)
+      .limit(limit)
+      .exec();
+
+    res.status(200).json(contacts);
   } catch (error) {
     const { status = 500, message = "Internal server error" } = error;
     res.status(status).json({ status, message });
